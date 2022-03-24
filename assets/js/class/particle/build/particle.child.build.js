fd759252acdc03205cef7e11547447f792fe26af
bemuse01 <sequeland@gmail.com>
2022-03-24 14:09:17 +0900

    update

--- assets/js/class/particle/build/particle.child.build.js
+++ assets/js/class/particle/build/particle.child.build.js
@@ -9,11 +9,11 @@ export default class{
     constructor({group, renderer}){
         this.param = {
             w: 100,
-            h: 100,
+            h: 500,
             div: 0.05,
             color: 0xffffff,
             opacity: 1,
-            size: 10,
+            size: 2,
             rd: 0.5
         }
 
@@ -52,7 +52,8 @@ export default class{
             shader: Shader.position,
             uniforms: {
                 uTime: {value: null},
-                uLifeVelocity: {value: 0.001},
+                uRand: {value: null},
+                uLifeVelocity: {value: 0.01},
                 staticPosition: {value: staticPosition}
             }
         })
@@ -68,17 +69,17 @@ export default class{
                 const index = (j * width + i) * 4
 
                 // position x
-                data[index + 0] = Math.random()
+                data[index + 0] = Math.random() * 0.2 - 0.1
                 // position y
-                data[index + 1] = Math.random()
+                data[index + 1] = Math.random() * 0.2 - 0.1
                 // position z
-                data[index + 2] = Math.random()
+                data[index + 2] = Math.random() * 0.2 - 0.1
                 // life
                 data[index + 3] = Math.random()
 
-                staticPosition[index + 0] = Math.random()
-                staticPosition[index + 1] = Math.random()
-                staticPosition[index + 2] = Math.random()
+                staticPosition[index + 0] = THREE.Math.randFloat(0.005, 0.01)
+                staticPosition[index + 1] = 0
+                staticPosition[index + 2] = 0
                 staticPosition[index + 3] = 0
             }
         }
@@ -136,6 +137,7 @@ export default class{
         const time = window.performance.now()
 
         this.positionVariable.setUniform('uTime', time)
+        this.positionVariable.setUniform('uRand', Math.random())
 
         this.object.setUniform('tPosition', this.gpuCompute.getCurrentRenderTarget(this.positionVariable.get()).texture)
     }
--- assets/js/class/particle/shader/particle.child.shader.js
+++ assets/js/class/particle/shader/particle.child.shader.js
@@ -34,26 +34,34 @@ export default {
     position: `
         uniform float uLifeVelocity;
         uniform float uTime;
+        uniform float uRand;
         uniform sampler2D staticPosition;
 
         const float PI = ${Math.PI};
 
         ${ShaderMethod.snoise4D()}
+        ${ShaderMethod.rand()}
+        ${ShaderMethod.executeNormalizing()}
 
         void main(){
             vec2 uv = gl_FragCoord.xy / resolution.xy;
 
             vec4 pos = texture(tPosition, uv);
-            vec4 staticPos = texture(staticPosition, uv);
+            // vec4 staticPos = texture(staticPosition, uv);
 
-            float n1 = snoise4D(vec4(pos.xyz, uTime * 0.001));
-            float n2 = snoise4D(vec4(pos.xyz, uTime * 0.001));
+            float n1 = snoise4D(vec4(pos.xyz * 0.3, uTime * 0.002));
+            float n2 = snoise4D(vec4(pos.xyz * 0.4, uTime * 0.0025));
 
-            float theta = 2.0 * PI * n1;
-            float phi = acos(2.0 * n2 - 1.0);
-            float velX = cos(theta) * sin(phi) * 0.5;
-            float velY = sin(theta) * sin(phi) * 0.5;
-            float velZ = cos(phi) * 0.5;
+            float pn1 = executeNormalizing(n1, 0.0, 1.0, -1.0, 1.0);
+            float pn2 = executeNormalizing(n2, 0.0, 1.0, -1.0, 1.0);
+
+            float scale = 0.25;
+            // float theta = 2.0 * PI * pn1;
+            float theta = 1.0 * PI * pn1;
+            float phi = acos(2.0 * pn2 - 1.0);
+            float velX = cos(theta) * sin(phi) * scale;
+            float velY = sin(theta) * sin(phi) * scale;
+            float velZ = cos(phi) * scale;
 
             // position
             pos.xyz += vec3(velX, velY, velZ);
@@ -62,7 +70,10 @@ export default {
             pos.w -= uLifeVelocity;
 
             if(pos.w < 0.0){
-                pos.xyz = staticPos.xyz;
+                float x = rand(uv * uRand * 0.1) * 0.2 - 0.1;
+                float y = rand(uv * uRand * 0.2) * 0.2 - 0.1;
+                float z = rand(uv * uRand * 0.3) * 0.2 - 0.1;
+                pos.xyz = vec3(x, y, z);
                 pos.w = 1.0;
             }
 
